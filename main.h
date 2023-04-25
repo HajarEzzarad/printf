f26c192d6d7a4f38e9193beb2d50bebdf79aff0f
BTISSAME <btissamehlial196@@gmail.com>
2023-04-25 09:03:26 -0700

    task 0

--- main.h
+++ main.h
@@ -6,6 +6,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <limits.h>
+#define NULL_STRING "(null)"
+#define NUL '\0'
 
 /**
  * struct convert - a function that defines a structure
@@ -37,4 +39,14 @@ int print_rev(va_list lis);
 int print_rot13(va_list l);
 int _putchar(char c);
 
+int print_number(va_list ar);
+unsigned int len(unsigned int, int);
+char *reverse_string(char *);
+void write_base(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int print_unsigned_number(unsigned int n);
+int hex_check(int n, char x);
+int print_hexau(unsigned long int num);
+int is_non_alpha_num(char c);
+char *convert(unsigned long int n, int b, int lc);
 #endif
